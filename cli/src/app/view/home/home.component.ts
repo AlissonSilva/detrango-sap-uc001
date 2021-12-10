006f9329318d377db79d1ced0c4cdac2c525e5aa
AlissonSilva <alisson_sao@hotmail.com>
2021-12-10 16:43:31 -0300

    10/12/2021
    Alteração layout da tabela

--- cli/src/app/view/home/home.component.ts
+++ cli/src/app/view/home/home.component.ts
@@ -1,23 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  descricao: string;
+  tipo_item: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {tipo_item: 'Documentos', descricao: 'Hydrogen'},
+  {tipo_item: 'Documentos', descricao: 'Helium'},
+  {tipo_item: 'Outros', descricao: 'Lithium'},
+  {tipo_item: 'Documentos', descricao: 'Beryllium'},
+  {tipo_item: 'Outros', descricao: 'Boron'},
+  {tipo_item: 'Documentos', descricao: 'Carbon'},
+  {tipo_item: 'Outros', descricao: 'Nitrogen'},
+  {tipo_item: 'Outros', descricao: 'Oxygen'},
+  {tipo_item: 'Documentos', descricao: 'Fluorine'},
+  {tipo_item: 'Outros', descricao: 'Neon'},
 ];
 
 @Component({
@@ -26,7 +24,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name','action'];
+  displayedColumns: string[] = ['tipo_item', 'descricao','action'];
   dataSource = ELEMENT_DATA;
   constructor() { }
 
